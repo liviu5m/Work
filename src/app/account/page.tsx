c7ef60023abcc3d82ba51d681bcc1b2254e887d2
liviu5m <motpanliviuwork@gmail.com>
2024-12-29 14:17:01 +0200

    fixed

---
+++ src/app/account/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div>
+      <div className='py-40 bg-white container mx-auto'>
+        <div className="w-1/2">
+          <div>
+            <h1 className='text-2xl font-bold'>Update User Data</h1>
+          </div>
+          <div>
+            <form>
+              
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
